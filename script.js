13a49036a3b13a0ebbf667247afac5ebe560dcfe
Monika <raciam@oregonstate.edu>
2023-05-18 22:44:04 -0500

    update prompt text; add resources; comment out console logs used for testing

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ const uppercaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "
 const lowercaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const specialChars = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", "-", ",", ".", "/", "~", "}", "|", "{", "`", "_", "^", "[", "]", "\\", "@", "?", "="];
 
-// Variable to hold password length
+// Declare variable to hold user input for password length
 let passwordLength;
 // Declare empty array to hold user selected character set(s)
 let selectedCharSets = [];
+// Declare empty array to hold randomly generated characters
 let randomizedChars = [];
 let newPassword = [];
 
@@ -27,63 +28,62 @@ generateBtn.addEventListener("click", writePassword);
 
 // TO DO:
 function generatePassword() {
+    // Resource for while loop: https://stackoverflow.com/questions/6961095/how-can-i-continue-a-javascript-if-else-if-statement-until-i-receive-valid-inp
     // Prompt user for password length
     while(true) {
-        passwordLength = window.prompt("How many numbers?");
+        passwordLength = window.prompt("How long would you like your password to be? Choose a number between 8 and 128.");
          // Validate user input is a number
         if (isNaN(passwordLength)) {
-            alert("Not a number!");
+            alert("Oops! You did not enter a number. Try again.");
         // Validate user input is between 8 and 128 characters (inclusive)
         } else if (passwordLength < 8 || passwordLength > 128) {
-            alert("Not in range!");
+            alert("Oops! The number you entered is out of range. Try again.");
         } else
         break;
     }
     
-    // Confirm if user wants numeric characters
-    var confirmNumbers = confirm("Include numbers?");
-    console.log(confirmNumbers);
-    
-    // If true, add numeric characters to new array
+    // Confirm if user wants numbers
+    var confirmNumbers = confirm("Would you like to include numbers in your password?");
+    // If true, add numbers to new array
     if (confirmNumbers) {
         selectedCharSets = selectedCharSets.concat(numericChars);
     }
 
-    // Confirm is user wants uppercase characters
-    var confirmUppercase = confirm("Include uppercase letters?");
-    console.log(confirmUppercase);
-    // If true, add uppercase characters to new array
+    // Confirm is user wants uppercase letters
+    var confirmUppercase = confirm("Would you like to inclue uppercase letters in your password?");
+    // If true, add uppercase letters to new array
     if (confirmUppercase) {
         selectedCharSets = selectedCharSets.concat(uppercaseChars);
       }
     
     // Confirm if user wants lowercase characters
-    var confirmLowercase = confirm("Include lowercase letters?");
-    console.log(confirmLowercase);
-    // If true, add lowercase characters to new array
+    var confirmLowercase = confirm("Would you like to include lowercase letters in your password?");
+    // If true, add lowercase letters to new array
     if (confirmLowercase) {
         selectedCharSets = selectedCharSets.concat(lowercaseChars);
     }
   
     // Confirm if user wants special characters
-    var confirmSpecial = confirm("Include special characters?");
-    console.log(confirmSpecial);
+    var confirmSpecial = confirm("Would you like to include special characters in your password?");
     // If true, add special characters to new array
     if (confirmSpecial) {
         selectedCharSets = selectedCharSets.concat(specialChars);
     }
 
-    // Validate user selection includes at least one character set
+    // Validate user selection includes at least one character set; if not, stop 
     if ((!confirmUppercase) && (!confirmLowercase) && (!confirmNumbers) && (!confirmSpecial)) {
         return null;
     }
 
-    console.log(confirmNumbers, confirmUppercase, confirmLowercase, confirmSpecial);
-    console.log(selectedCharSets);
+    //console.log(confirmNumbers, confirmUppercase, confirmLowercase, confirmSpecial);
+    //console.log(selectedCharSets);
 
-    for(var i = 0; i <passwordLength; i++) {
+    // Resource for getting a random number: https://git.bootcampcontent.com/University-of-Minnesota/UofM-VIRT-FSF-PT-04-2023-U-LOLC-ENTG/-/blob/main/03-JavaScript/01-Activities/28-Stu_Mini-Project/Main/script.js
+    // Randomly select a character from user character choice(s); add characters to final array
+    for (var i = 0; i <passwordLength; i++) {
         randomizedChars = selectedCharSets[Math.floor(Math.random() * selectedCharSets.length)]
         newPassword.push(randomizedChars);
       }
+      // Turn array into string
       return newPassword.join('');
     }
